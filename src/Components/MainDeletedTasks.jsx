93c8ef064a35409382d24de845a40b4c6bc51fb3
ricardoelias98 <uc2016225298@student.uc.pt>
2024-03-19 14:43:28 +0000

    cc

--- src/Components/MainDeletedTasks.jsx
+++ src/Components/MainDeletedTasks.jsx
@@ -1,23 +1,76 @@
 import React from "react";
 import { tasksStore } from "../stores/TasksStore";
 import Task from "./Task";
+import { userStore } from "../stores/UserStore";
+import { useEffect } from "react";
 
 function MainDeletedTasks() {
+  const token = userStore.getState().token;
   const tasks = tasksStore.getState().tasks;
   const deletedTasks = tasks.filter((task) => task.active === false);
 
+  useEffect(() => {
+    
+  }, [tasks]);
+
+  const handleDragStart = (event, username) => {
+    event.dataTransfer.setData("user_id", username);
+  };
+
+
+  const handleDropRestore = (event) => {
+    const taskId = event.dataTransfer.getData("data_id");
+    console.log(taskId);
+    fetch(`http://localhost:8080/project4backend/rest/task/restore/${taskId}`, {
+      method: "PUT",
+      headers: {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        token: token,
+      },
+    })
+      .then(async function (response) {
+        if (response.status === 400) {
+          alert("Task with this token is not found");
+        } else if (response.status === 200) {
+            alert("Task restored");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching tasks:", error);
+      });
+  };
+
+  const handleDropDelete = (event) => {
+    const taskId = event.dataTransfer.getData("data_id");
+    console.log(taskId);
+    fetch(`http://localhost:8080/project4backend/rest/task/delete/${taskId}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        token: token,
+      },
+    })
+      .then(async function (response) {
+        if (response.status === 400) {
+          alert("Task with this token is not found");
+        } else if (response.status === 200) {
+            alert("Task permanently deleted")
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching tasks:", error);
+
+      });
+  }
   return (
     <div className="board">
       <div className="total-column">
         <div className="column-header" id="deleted-header">
           <h2>Deleted</h2>
         </div>
-        <div
-          className="board-container"
-          id="deleted-container"
-          //onDragOver={(event) => event.preventDefault()}
-          //onDrop={(event) => handleDrop(event, "developer")}
-        >
+        <div className="board-container" id="deleted-container">
           <section className="board-column" id="deleted-column">
             {deletedTasks.map((task) => (
               <Task
@@ -30,7 +83,7 @@ function MainDeletedTasks() {
                 startDate={task.startDate}
                 endDate={task.endDate}
                 status={task.status}
-                //onDragStart={(event) => handleDragStart(event, task.id)}
+                onDragStart={(event) => handleDragStart(event, task.id)}
               />
             ))}
           </section>
@@ -43,8 +96,8 @@ function MainDeletedTasks() {
         <div
           className="board-container"
           id="deletePerm-container"
-          //onDragOver={(event) => event.preventDefault()}
-          //onDrop={(event) => handleDrop(event, "user")}
+          onDragOver={(event) => event.preventDefault()}
+          onDrop={(event) => handleDropRestore(event)}
         >
           <section className="board-column" id="deletePerm-column"></section>
         </div>
@@ -56,8 +109,8 @@ function MainDeletedTasks() {
         <div
           className="board-container"
           id="restore-container"
-          //onDragOver={(event) => event.preventDefault()}
-          //onDrop={(event) => handleDrop(event, "user")}
+          onDragOver={(event) => event.preventDefault()}
+          onDrop={(event) => handleDropDelete(event)}
         >
           <section className="board-column" id="restore-column"></section>
         </div>
